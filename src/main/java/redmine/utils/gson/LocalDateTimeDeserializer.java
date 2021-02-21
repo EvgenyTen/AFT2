3ee396d710f2e2501d73b272aac5bd2ff5285c2f
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-21 23:59:21 +0200

    release

--- src/main/java/redmine/utils/gson/LocalDateTimeDeserializer.java
+++ src/main/java/redmine/utils/gson/LocalDateTimeDeserializer.java
@@ -4,14 +4,16 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import lombok.Data;
 
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Data
 public class LocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
 
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     @Override
     public LocalDateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
--- src/main/java/redmine/utils/gson/LocalDateTimeSerializer.java
+++ src/main/java/redmine/utils/gson/LocalDateTimeSerializer.java
@@ -4,14 +4,15 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-
+import lombok.Data;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Data
 public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
 
-    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     @Override
     public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
