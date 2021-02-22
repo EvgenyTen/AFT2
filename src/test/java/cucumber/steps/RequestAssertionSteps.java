2f54c3eae0a3279d7e1f51657db7bd3b3cccbd66
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-22 10:42:11 +0200

    release prepared query В теле содержится информация о пользователе

--- src/test/java/cucumber/steps/RequestAssertionSteps.java
+++ src/test/java/cucumber/steps/RequestAssertionSteps.java
@@ -116,8 +116,8 @@ public class RequestAssertionSteps {
     @И("В теле содержится информация о пользователе {string}")
     public void assertUserInformationExistInDb(String stashId) {
         User userContext = Context.get(stashId, User.class);
-        String query = String.format("select * from users where login='%s'", userContext.getLogin());
-        List<Map<String, Object>> result = Manager.dbConnection.executeQuery(query);
+        String query = "select * from users where login=?";
+        List<Map<String, Object>> result = Manager.dbConnection.executePreparedQuery(query,userContext.getLogin());
         Assert.assertEquals(result.size(), 1, "Проверка размера результата");
         Map<String, Object> dbUser = result.get(0);
         Asserts.assertEquals(dbUser.get("login"), userContext.getLogin());
