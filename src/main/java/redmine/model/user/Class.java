02e3f69495bc0e5c7e170b9a8bf74ba1844c56f5
EvgenyT <evgeny.tenenbaum@gmail.com>
2021-02-23 12:41:28 +0200

    release v2

---
+++ src/main/java/redmine/model/user/Class.java
@@ -0,0 +1,10 @@
+package redmine.model.user;
+
+import redmine.db.requests.UserRequests;
+
+public class Class {
+    public static void main(String[] args){
+        System.out.println(UserRequests.getUserByLogin("Eogibdmwt"));
+
+    }
+}
